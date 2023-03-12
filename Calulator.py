12a81750ce5ee45bd0f84ccd122cbe717f74fe81
Mikołaj <127327766+1Kirako@users.noreply.github.com>
2023-03-12 21:24:00 +0100

    Create Calulator.py

---
+++ Calulator.py
@@ -0,0 +1,41 @@
+from math import *
+# Symbols of operations: + -- Addition - -- Subtraction * -- Multiplication / -- Division ^ -- Exponentiation log -- Logarithm
+print("Welcome to Kirako's Calculator, "
+      "Current version: 1.2")
+operation = input("Enter the symbol of which operation do you want to do: ")
+
+if operation == "+":  # Addition
+    num1 = input("First number: ")
+    num2 = input("Second number: ")
+
+    print((float(num1)) + (float(num2)))
+else:
+    if operation == "-":  # Subtraction
+        num1 = input("First number: ")
+        num2 = input("Second number: ")
+
+        print((float(num1)) - (float(num2)))
+    else:
+        if operation == "*":  # Multiplication
+            num1 = input("First number: ")
+            num2 = input("Second number: ")
+
+            print((float(num1)) * (float(num2)))
+        else:
+            if operation == "^":  # Exponentiation
+                num1 = input("First number: ")
+                num2 = input("Second number: ")
+                print(pow((float(num1)), (float(num2))))
+            else:
+                if operation == "log":  # Logarithm
+                    num1 = input("First number: ")
+                    num2 = input("Second number: ")
+                    print(log(float(num1), (float(num2))))
+                else:
+                    if operation == "/":  # Division pt.1
+                        num1 = input("First number: ")
+                        num2 = input("Second number: ")
+                        if num2 == "0":  # Division by 0
+                            print("Error")
+                        else:  # Division pt.2
+                            print((float(num1)) / (float(num2)))
