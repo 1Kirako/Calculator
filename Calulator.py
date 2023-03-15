9c6c2a2e34673a193b39edd21e746a946fc2f76a
Mikołaj <127327766+1Kirako@users.noreply.github.com>
2023-03-15 07:13:22 +0100

    Update Calulator.py

--- Calulator.py
+++ Calulator.py
@@ -1,33 +1,33 @@
 from math import *
-# Symbols of standard calculator operations: + -- Addition - -- Subtraction * -- Multiplication / -- Division 
+# Symbols of standard calculator operations: + -- Addition - -- Subtraction * -- Multiplication / -- Division
 # Symbols of other operations ^ -- Exponentiation log -- Logarithm
-print("Welcome to Kirako's Calculator, ")
-print("Current version: 2.1")
+# Welcome to the Calculator
+# Current version: 2.2
 operation = input("Enter the symbol of which operation do you want to do: ")
 if operation == "+":  # Addition
-  num1 = float(input("First number: "))
-  num2 = float(input("Second number: "))
-  print(num1 + num2)
+    num1 = float(input("First number: "))
+    num2 = float(input("Second number: "))
+    print(num1 + num2)
 elif operation == "-":  # Subtraction
     num1 = float(input("First number: "))
     num2 = float(input("Second number: "))
     print(num1 - num2)
 elif operation == "*":  # Multiplication
-        num1 = float(input("First number: "))
-        num2 = float(input("Second number: "))
-        print(num1 * num2)
+    num1 = float(input("First number: "))
+    num2 = float(input("Second number: "))
+    print(num1 * num2)
 elif operation == "^":  # Exponentiation
-                num1 = float(input("First number: "))
-                num2 = float(input("Second number: "))
-                print(pow(num1, num2))
+    num1 = float(input("First number: "))
+    num2 = float(input("Second number: "))
+    print(pow(num1, num2))
 elif operation == "log":  # Logarithm
-                    num1 = float(input("First number: "))
-                    num2 = float(input("Second number: "))
-                    print(log(num1, num2))
+    num1 = float(input("First number: "))
+    num2 = float(input("Second number: "))
+    print(log(num1, num2))
 elif operation == "/":  # Division pt.1
-                        num1 = float(input("First number: "))
-                        num2 = float(input("Second number: "))
-                        if num2 == "0":  # Division by 0
-                            print("Error")
-                        else:  # Division pt.2
-                            print(num1 / num2)
+    num1 = float(input("First number: "))
+    num2 = float(input("Second number: "))
+    if num2 == "0":  # Division by 0
+        print("Error")
+    else:  # Division pt.2
+        print(num1 / num2)
