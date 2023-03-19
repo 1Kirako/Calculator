d316cb2507c507e708d9391e2b9f411531356c11
Mikołaj <127327766+1Kirako@users.noreply.github.com>
2023-03-19 12:11:27 +0100

    Update Calulator.py

--- Calulator.py
+++ Calulator.py
@@ -1,6 +1,6 @@
 from math import *
 # Symbols of standard calculator operations: + -- Addition - -- Subtraction * -- Multiplication / -- Division
-# Symbols of other operations ^ -- Exponentiation log -- Logarithm sqrt - Square root
+# Symbols of other operations ^ -- Exponentiation log -- Logarithm // - Square root
 # Welcome to the Calculator
 # Current version: 3.0
 operation = input("Enter the symbol of which operation do you want to do: ")
@@ -24,7 +24,7 @@ elif operation == "log":  # Logarithm
     num1 = float(input("First number: "))
     num2 = float(input("Second number: "))
     print(log(num1, num2))
-elif operation == "sqrt":  # Square root
+elif operation == "//":  # Square root
     num1 =float(input("Number: "))
     print(sqrt(num1))
 elif operation == "/":  # Division pt.1
