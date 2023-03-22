33cafb9bb8bf00fed855ed2035d78534eb7e82f1
Mikołaj <127327766+1Kirako@users.noreply.github.com>
2023-03-22 18:35:55 +0100

    Optimization
    So I found a way to reduce my code by like 10-20 lines, and I implemented It.

--- Calulator.py
+++ Calulator.py
@@ -2,35 +2,27 @@ from math import *
 # Symbols of standard calculator operations: + -- Addition - -- Subtraction * -- Multiplication / -- Division
 # Symbols of other operations ^ -- Exponentiation log -- Logarithm // - Square root
 # Welcome to the Calculator
-# Current version: 3.1
+# Current version: 4.1
 operation = input("Enter the symbol of which operation do you want to do: ")
-if operation == "+":  # Addition
-    num1 = float(input("First number: "))
-    num2 = float(input("Second number: "))
-    print(num1 + num2)
-elif operation == "-":  # Subtraction
-    num1 = float(input("First number: "))
-    num2 = float(input("Second number: "))
-    print(num1 - num2)
-elif operation == "*":  # Multiplication
-    num1 = float(input("First number: "))
-    num2 = float(input("Second number: "))
-    print(num1 * num2)
-elif operation == "^":  # Exponentiation
-    num1 = float(input("First number: "))
-    num2 = float(input("Second number: "))
-    print(pow(num1, num2))
-elif operation == "log":  # Logarithm
-    num1 = float(input("First number: "))
-    num2 = float(input("Second number: "))
-    print(log(num1, num2))
-elif operation == "//":  # Square root
-    num1 =float(input("Number: "))
+if operation == "//":  # Square root
+    num1 = float(input("Number: "))
     print(sqrt(num1))
-elif operation == "/":  # Division pt.1
+else:
     num1 = float(input("First number: "))
     num2 = float(input("Second number: "))
-    if num2 == "0":  # Division by 0
-        print("Error")
-    else:  # Division pt.2
+    if operation == "+":  # Addition
+        print(num1 + num2)
+    if operation == "-":  # Subtraction
+        print(num1 - num2)
+    if operation == "*":  # Multiplication
+        print(num1 * num2)
+    if operation == "^":  # Exponentiation
+        print(pow(num1, num2))
+    if operation == "log":  # Logarithms
+        print(log(num1, num2))
+    if operation == "/":  # Division
+        if num2 == 0:
+            print("Error!")
+        else:
+            print(num1 / num2)
         print(num1 / num2)
